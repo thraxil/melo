91c9f626defc6004c50981ea30d09ce5ca7fecd7
Anders Pearson <anders@thraxil.org>
2021-12-28 11:25:05 +0000

    controller tests

--- test/melo_web/controllers/page_controller_test.exs
+++ test/melo_web/controllers/page_controller_test.exs
@@ -5,4 +5,27 @@ defmodule MeloWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Pixelvore"
   end
+
+  test "GET /random", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Pixelvore"
+  end
+
+  test "admin needs login", %{conn: conn} do
+    conn = get(conn, "/admin")
+    assert redirected_to(conn) == "/auth/auth0"
+  end
+
+  test "GET an image", %{conn: conn} do
+    image = Melo.MainFixtures.insert_image()
+    conn = get(conn, "/image/" <> to_string(image.id))
+    assert html_response(conn, 200) =~ image.ahash
+  end
+
+  test "inserted image should show up on index", %{conn: conn} do
+    image = Melo.MainFixtures.insert_image()
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ image.ahash
+    assert html_response(conn, 200) =~ to_string(image.id)
+  end
 end
--- test/support/fixtures/main_fixtures.ex
+++ test/support/fixtures/main_fixtures.ex
@@ -1,2 +1,14 @@
 defmodule Melo.MainFixtures do
+  alias Melo.Repo
+
+  def insert_image(attrs \\ %{}) do
+    changes = Dict.merge(%{
+          ahash: "asdfasdfasd",
+          ext: ".jpg",
+          url: "https://example.com",
+                         }, attrs)
+    %Melo.Main.Image{}
+    |> Melo.Main.Image.changeset(changes)
+    |> Repo.insert!()
+  end
 end
